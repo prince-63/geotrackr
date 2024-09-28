d75f3c6fb17784a67f0659c1093a709d41573f44
Prince Kumar Prasad <prince08833@gmail.com>
2024-09-28 07:54:38 +0530

    delete some unused files

--- server/services/role/add-role.js
+++ server/services/role/add-role.js
@@ -9,13 +9,27 @@ const addRole = async (req, res) => {
     return errorResponseHandler(res, 400, 'fail', 'Please provide a role name');
   }
 
-  const role = await prisma.role.create({
-    data: {
-      roleName,
-    },
-  });
-
-  return responseHandler(res, 201, 'success', 'Role added successfully', role);
+  try {
+    const role = await prisma.role.create({
+      data: {
+        roleName,
+      },
+    });
+    return responseHandler(
+      res,
+      201,
+      'success',
+      'Role added successfully',
+      role
+    );
+  } catch (error) {
+    return errorResponseHandler(
+      res,
+      500,
+      'error',
+      'An error occurred while adding role'
+    );
+  }
 };
 
 export default addRole;
